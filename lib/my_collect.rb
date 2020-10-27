a8cab6af8e9a9f4785fd6d0079ed6a3694a78a9f
DariasKehl <neocount@gmail.com>
2020-10-27 15:56:04 -0400

    commit tree 62c3385fac35b21f909004d1bdffef197488288b

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection) 
+    l = 0
+    new_col = Array.new
+    while l < collection.length
+        new_col << yield(collection[l])
+        l += 1
+    end
+    new_col
+end
